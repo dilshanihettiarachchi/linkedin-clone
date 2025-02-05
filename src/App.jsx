ec15797e000bbf2e85d82c4398433d361f228be6
Dilshani Hettiarachchi <dilshani.madhuwanthi151@gmail.com>
2025-02-05 10:45:37 +0530

    add signin and signup components with syles and integrate authentication state management with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,55 @@
-import Feed from "./components/Feed";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import Widget from "./components/Widget";
+import Home from "./pages/Home";
+import { selectUser, login, logout } from "./redux/userSlice";
+import { useEffect } from "react";
+import { auth, db } from "./firebase";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import { Route, Routes } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userDoc = await getDoc(doc(db, "users", userAuth.uid));
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        
+        dispatch(login({
+          email: userData.email,
+          uid: userData.uid,
+          displayName: userData.displayName, // Use Firestore value
+          currentPosition: userData.currentPosition, // Use Firestore value
+          photoURL: userData.photoURL,
+        }));
+      }
+      } else {
+        dispatch(logout());
+      }
+    })
+  },[])
 
   return (
-    <div className="app">
-      <Header />
-      <div className="app-body">
-        <Sidebar />
-        <Feed />
-        <Widget />
-      </div>
-    </div>
+        <div className="app">
+          <Header />
+          <Routes>
+            {!user ? (
+              <>
+                <Route path="/" element={ <SignIn /> } />
+                <Route path="/login" element={ <SignIn /> } />
+                <Route path="/register" element={ <SignUp /> } />
+              </>
+            ) : (
+              <Route path="/" element={ <Home /> } />
+            )}
+            <Route path="*" element={ <SignIn /> } />
+          </Routes>
+        </div>
   )
 }
 
