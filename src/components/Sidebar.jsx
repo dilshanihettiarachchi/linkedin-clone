497ae75f9dab5e18ec905bfe1e2ecd719bcd1b12
Dilshani Hettiarachchi <dilshani.madhuwanthi151@gmail.com>
2025-02-05 11:19:42 +0530

    add styles to the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ export default function Sidebar() {
     <div className="sidebar">
       <div className="sidebar-top">
         <img 
-          src="https://images.unsplash.com/photo-1487088678257-3a541e6e3922?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          src="https://images.pexels.com/photos/6113665/pexels-photo-6113665.jpeg?auto=compress&cs=tinysrgb&w=600" 
           alt="Cover image" 
         />
         <div className="sidebar-user-details">
